318f4bf1c3d55a81ff73bc8cd790683118c3af4a
Nishit Majithia <nishit.nm@gmail.com>
2021-05-11 17:47:44 +0530

    Add scan by pincode functionality

--- vaccine_scan.py
+++ vaccine_scan.py
@@ -1,4 +1,5 @@
 import urllib3
+import argparse
 import json
 from playsound import playsound
 from datetime import datetime
@@ -50,15 +51,68 @@ def count_18plus_vaccine_scan(district):
         print("--------------------------------")
 
 
-while(True):
-    BBMP = 294
-    BLR_URBAN = 265
-    BLR_RULAR = 276
-    try:
-        count_18plus_vaccine_scan(BLR_URBAN)
-        count_18plus_vaccine_scan(BBMP)
-        count_18plus_vaccine_scan(BLR_RULAR)
-        time.sleep(30)
-    except Exception as e:
-        print(e)
-        time.sleep(30)
+
+def count_18plus_vaccine_scan_pincode(pincode):
+    dt = datetime.now().strftime("%d-%m-%Y")
+    dt_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S %Z%z")
+    uri = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=" + str(pincode) + "&date=" + dt
+    print(uri)
+    http = urllib3.PoolManager()
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    }
+    r = http.request('GET', uri, headers=headers)
+    slots = json.loads(r.data)
+    centers = slots['centers']
+    availability = 0
+    total_availability = 0
+    available_center_name=set()
+    for center in centers:
+        for session in center['sessions']:
+            total_availability += session['available_capacity']
+            if(session['min_age_limit'] == 18):
+                availability += session['available_capacity']
+                available_center_name.add(center['name'])
+    if availability > 0:
+        for center in available_center_name:
+            print("################################")
+            print("For 18+: Pincode:{} and available center:{}".format(pincode,center))
+            print("################################")
+
+        print("Time:{} pincode:{} Available capacity for 18+:{} Total availability:{}".format(dt_time,pincode,availability,total_availability))
+        print("--------------------------------")
+        playsound('./file_example_WAV_1MG.wav')
+    else:
+        print("Time:{} Pincode:{} Available capacity for 18+:{} Total availability:{}".format(dt_time,pincode,availability,total_availability))
+        print("--------------------------------")
+
+
+parser = argparse.ArgumentParser(description='''--Vaccine Scanner--''')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-d', '--district', type=int, nargs='*', action='append', help='Add district codes')
+group.add_argument('-p', '--pincode', type=int, nargs='*', action='append', help='Add pincode')
+args = parser.parse_args()
+
+if not args.pincode:
+    while(True):
+        #BBMP = 294
+        #BLR_URBAN = 265
+        #BLR_RULAR = 276
+        try:
+            for district in args.district[0]:
+                count_18plus_vaccine_scan(district)
+            time.sleep(30)
+        except Exception as e:
+            print(e)
+            time.sleep(30)
+
+else:
+    while(True):
+        try:
+            for pincode in args.pincode[0]:
+                count_18plus_vaccine_scan_pincode(pincode)
+            time.sleep(30)
+        except Exception as e:
+            print(e)
+            time.sleep(30)
